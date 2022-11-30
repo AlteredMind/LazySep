94fd1d700a4e44851342a047a6bb055e8eb61d64
Matteo Ripamonti <m.ripamonti88@gmail.com>
2022-11-30 08:06:02 +0100

    Add banner and minor fix.

--- CSV_separator.ps1
+++ CSV_separator.ps1
@@ -1,9 +1,25 @@
 
 
+    Write-Host "`n"
+    Write-Host " ____________________________________________________________________________ " -ForegroundColor Red 
+    Write-Host "|                                                                            |" -ForegroundColor Red 
+    Write-Host "|                                 L A Z Y - S E P                            |" -ForegroundColor Red 
+    Write-Host "|                                                                            |" -ForegroundColor Red
+    Write-Host "|                                 Version 2022.11                            |" -ForegroundColor Red 
+    Write-Host "|____________________________________________________________________________|" -ForegroundColor Red 
+    Write-Host "|                                                                            |" -ForegroundColor Red 
+    Write-Host "|                                   Created by                               |" -ForegroundColor Red 
+    Write-Host "|                                Matteo Ripamonti                            |" -ForegroundColor Red 
+    Write-Host "|____________________________________________________________________________|" -ForegroundColor Red 
+    Write-Host ""
+    Write-Host "`n"
+
+Write-Host "All CSV files will be modified."    
+Write-Host "Please Drag or Add here the full-path folder :"
+
 #Path Mandatory
 param( [Parameter(Mandatory=$true)] $path)
     "directory: " + $path
-
 
 #Environment variables
 $DirectoryToCreate = "modified"
